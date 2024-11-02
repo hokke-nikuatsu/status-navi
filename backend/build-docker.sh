5cc85b89c4e13ec5b13a690c415bb4717aed9028
HoshiShuto <s.hoshi@secure-navi.com>
2024-11-03 02:14:36 +0900

    Make Docker build compatible across platforms

--- backend/build-docker.sh
+++ backend/build-docker.sh
@@ -8,8 +8,7 @@ aws_account_id=$(aws --profile $aws_profile sts get-caller-identity --query 'Acc
 aws --profile $aws_profile --region $aws_region ecr get-login-password | docker login --username AWS --password-stdin $aws_account_id.dkr.ecr.$aws_region.amazonaws.com
 
 # Build image
-docker build -t $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$resource_name .
-
+docker build --platform linux/amd64 --provenance=false -t $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$resource_name .
 
 # Push image
 docker push $aws_account_id.dkr.ecr.$aws_region.amazonaws.com/$resource_name
